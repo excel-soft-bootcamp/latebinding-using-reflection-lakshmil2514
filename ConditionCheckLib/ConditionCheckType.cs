91bd24c7e01459da9f343b22e122676317972234
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-07-20 15:08:31 +0530

    Add files via upload

---
+++ ConditionCheckLib/ConditionCheckType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConditionCheckLib
+{
+    public class ConditionCheckType
+    {
+        public static void CheckForNullIsClassIsStatic(System.Type levelClassRef, string methodName, object[] argument)
+        {
+            if (levelClassRef != null)
+            {
+                if (levelClassRef.IsClass)
+                {
+                    Object objRef = System.Activator.CreateInstance(levelClassRef);
+                    System.Reflection.MethodInfo _methodRef = levelClassRef.GetMethod(methodName);
+
+                    if (!_methodRef.IsStatic)
+                    {
+                        if (methodName == "Play")
+                        {
+                            object result = _methodRef.Invoke(objRef, argument);
+                            Console.WriteLine(result.ToString());
+                        }
+                        else if (methodName == "Start")
+                        {
+                            object result = _methodRef.Invoke(objRef, argument);
+                            Console.WriteLine(result.ToString());
+                        }
+                        else if (methodName == "Begin")
+                        {
+                            object result = _methodRef.Invoke(objRef, argument);
+                            Console.WriteLine(result.ToString());
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+    }
+}
